db3d98be49cd17768c036b52be203547cb438a46
Lisbet-24 <evelinparra663@gmail.com>
2025-02-07 18:59:32 -0500

    Agregando columna de Imagen en lista de productos

--- View/Producto/listaProducto.php
+++ View/Producto/listaProducto.php
@@ -4,13 +4,14 @@ echo "<a href='index.php?entity=Producto&action=insertar' class='btn btn-success
 
 echo "<table class='table table-striped'>";
 echo "<thead><tr>
-        <th>ID</th><th>Nombre</th><th>Precio</th><th>Stock</th><th>Acciones</th>
+        <th>ID</th><th>Nombre</th><th>Imagen</th><th>Precio</th><th>Stock</th><th>Acciones</th>
       </tr></thead><tbody>";
 
 foreach ($listadoelementos as $producto) {
     echo "<tr>";
     echo "<td>{$producto['id']}</td>";
     echo "<td>{$producto['nombre']}</td>";
+    echo "<td><img src='{$producto['imagen']}' width='100' height='100' style='object-fit: cover;'></td>";
     echo "<td>{$producto['precio']}</td>";
     echo "<td>{$producto['stock']}</td>";
     echo "<td>
